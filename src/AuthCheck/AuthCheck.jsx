13f904b6f37ce14acd0c4abcdfca816265cc80f3
Affan <affanqaisar879@gmail.com>
2025-10-01 17:23:51 +0500

    update

---
+++ src/AuthCheck/AuthCheck.jsx
@@ -0,0 +1,18 @@
+import { Navigate, useLocation } from "react-router";
+
+const AuthCheck = ({children}) => {
+    const isAuthentication = true;
+    const location = useLocation();
+
+    if(!isAuthentication){
+        return <Navigate to='auth/login' state={
+            {
+                from: location
+            }
+        } />
+    }
+
+    return children;
+};
+
+export default AuthCheck;
